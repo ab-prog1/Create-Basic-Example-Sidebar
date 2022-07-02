9e2db21c87a576d2068468d326621371a02a4a0b
Abolfazl Ghorbani <ab.ghorbani28@gmail.com>
2022-07-02 22:47:56 +0200

    Create-Basic-Example-Sidebar

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import {FaYoutube,FaFacebook,FaTwitter,FaLinkedin,FaHome,FaUserFriends,FaFolderOpen,FaCalendarAlt,FaWpforms} from 'react-icons/fa'
+
+
+export const links = [
+     {
+          id: 1,
+          url: '/',
+          text: 'Home',
+          icon: <FaHome />
+     },
+     {
+          id: 2,
+          url: '/tean',
+          text: 'colleagues',
+          icon: <FaUserFriends />
+     },
+     {
+          id: 3,
+          url: '/projects',
+          text: 'Projects',
+          icon: <FaFolderOpen />
+     },
+     {
+          id: 4,
+          url: '/docs',
+          text: 'Docuents',
+          icon: <FaWpforms />
+     },
+   
+]
